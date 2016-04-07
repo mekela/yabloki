2ad6b75842dadf9a005b87f8586cd9a798acd74c
alex <sania_free@i.ua>
2016-04-07 17:03:43 +0300

    last page

--- js/common.js
+++ js/common.js
@@ -15,4 +15,27 @@ $(document).ready(function() {
 	  nextText: 'Onward →',
 	  prevText: '← Go back'
 	});
+
+	//datepicker
+	
+	$.datepick.setDefaults($.datepick.regionalOptions['ru']);
+
+	
+
+	function updateLinked(dates) { 
+	    $('#linkedMonth').val(dates.length ? dates[0].getMonth() + 1 : ''); 
+	    $('#linkedDay').val(dates.length ? dates[0].getDate() : ''); 
+	    $('#linkedYear').val(dates.length ? dates[0].getFullYear() : ''); 
+	} 
+	 
+	$('.date').datepick({ 
+	    alignment: 'bottomRight', onSelect: updateLinked
+	 }); 
+	 
+	$('#linkedMonth,#linkedDay,#linkedYear').change(function() { 
+	    $('.date').datepick('setDate', new Date( 
+	        parseInt($('#linkedYear').val(), 10), 
+	        parseInt($('#linkedMonth').val(), 10) - 1, 
+	        parseInt($('#linkedDay').val(), 10))); 
+	});
 });
